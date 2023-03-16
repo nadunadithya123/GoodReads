fb8e49f51cf7741f9c33d75f774add8b2996756d
Nadun Wijerathna <nadunadithya123@gmail.com>
2023-03-17 00:26:57 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import axios from 'axios';
 function App() {
   const [feedback, setFeedback] = useState('')
   const [keyValue, setKeyValue] = useState('')
-  useEffect(() => {
+  
+  const handleClick = async () => {
+    const res =await axios.post('http://localhost:8081/predict',{
+    feedback:keyValue
+  })
+  console.log("res",res)
+  setFeedback(res.data)
+  }
 
-    const getResponse = async () => {
-      const res =await axios.post('http://localhost:8081/predict',{
-      feedback:keyValue
-    })
-    console.log("res",res)
-    setFeedback(res.data)
-    }
-    setTimeout(() => {
-      if(keyValue.length > 6)
-          getResponse()
-
-    },3000)
-  },[keyValue])
   return (
     <div className="App">
+
+<div style={{display:"flex", flexDirection:"column"}}>
       <input placeholder='Enter your feedback' value={keyValue} onChange={((e) => setKeyValue(e.target.value) ) }/>
+      <button onClick={handleClick}> Submit</button>
+
+      </div>
       <h1>{feedback}</h1>
+      
     </div>
   );
 }
